81beee03945601e0333bc36bd6559975441637f3
Shane Donnelly <shanejdonnelly@gmail.com>
2013-03-24 15:45:36 -0400

    fix indentation

--- timberland_tech/template.php
+++ timberland_tech/template.php
@@ -4,7 +4,7 @@ global $options;
 foreach ($options as $value) {
     if (get_option( $value['id'] ) === FALSE) { $value['id'] = $value['std']; }
     else { $value['id'] = get_option( $value['id'] ); }
-    }
+}
 ?>
 <?php 
 
@@ -12,35 +12,35 @@ get_template_part('header');
 
 ?>
 <div id="access" class="<?php ?>">
-	
-		<?php wp_nav_menu( array( 'theme_location' => 'tbl_nav','container_id' => 'nav', 'menu_class' => 'menu' ) ); ?>
 
-	</div>
+        <?php wp_nav_menu( array( 'theme_location' => 'tbl_nav','container_id' => 'nav', 'menu_class' => 'menu' ) ); ?>
+
+    </div>
 <!-- this is the category.php file -->
 
-	<div id="container">
-		
-		<div id="categories">
-			
+    <div id="container">
 
-		
-      
-      <?php
-				$description_array = categoryDescriptions($cat);
-				sidebarMenu($cat, $description_array);
-								
-			?>			
-		</div>
-	
-		<div id="contentContainer">
+        <div id="categories">
 
-			<div id="content">
-			
-				<!-- post is dynamically appended here-->
 
-			</div><!-- #content -->
-		</div>
-		<div style="clear:both;"></div> 
-	</div><!-- #container -->
-	
+
+
+<?php
+$description_array = categoryDescriptions($cat);
+sidebarMenu($cat, $description_array);
+
+?>			
+        </div>
+
+        <div id="contentContainer">
+
+            <div id="content">
+
+                <!-- post is dynamically appended here-->
+
+            </div><!-- #content -->
+        </div>
+        <div style="clear:both;"></div> 
+    </div><!-- #container -->
+
 <?php get_footer(); ?>
