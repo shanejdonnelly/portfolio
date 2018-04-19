f2b83d81eba644e53da79d2ef88c938d3a295b2e
Shane Donnelly <shane@likeablelocal.com>
2018-04-19 16:38:10 -0400

    remove comment

--- jquery-dropdown.js
+++ jquery-dropdown.js
@@ -7,10 +7,6 @@
   // The actual plugin constructor
   function Plugin ( element, options ) {
     this.element = element;
-    // jQuery has an extend method which merges the contents of two or
-    // more objects, storing the result in the first object. The first object
-    // is generally empty as we don't want to alter the default options for
-    // future instances of the plugin
     this.settings = $.extend( {}, defaults, options );
     this._defaults = defaults;
     this._name = pluginName;
