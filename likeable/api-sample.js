55fc8c293d22994ce21e7935ce00d91c2f195c74
Shane Donnelly <shanejdonnelly@gmail.com>
2019-10-02 19:10:09 -0400

    add sample from snippet of a larger file - backend

---
+++ likeable/api-sample.js
@@ -0,0 +1,18 @@
+export function getAdAccountInsights(adAccountId, since, until) {
+  const Models = require('../../db/models');
+  return new Promise((resolve, reject) => {
+    const data = {
+      access_token: masterAccessToken,
+      fields: adDashboardInsightFields,
+      time_range: { since: moment(since).format('YYYY-MM-DD'), until: moment(until).format('YYYY-MM-DD') },
+      time_increment: 1
+    };
+    const url = `${fbApiVersion}/${adAccountId}/insights`;
+    FB.api(url, 'get', data, response => {
+      if (!response || response.error) {
+        return reject(response ? response.error : new Error('Failed to get ad account insights'));
+      }
+      return resolve(response.data);
+    });
+  });
+}
